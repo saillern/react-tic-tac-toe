d2c9906621c8315fac2d1e9dd299db27161a1dcc
saillern <schwarzesaillern@gmail.com>
2024-05-10 00:40:10 +0900

    Add header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const stateParams = {
+  circleTurn : true,
+  cells : new Array(9).fill(""),
+  turnCount : 0,
+  done : false
 }
 
-export default App;
+const STATE_MESSAGE = {
+    play : "processing",
+    win : "win",
+    draw : "drow",
+}
+
+const PATTERN = {
+    circle: "○",
+    cross: "×"
+}
+
+const WIN_PATTERN = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+]
+
+const Title = styled.h1`
+  font-size: 1.2rem;
+  text-align: center;
+`;
+
+const Container = styled.div`
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  display :flex;
+`
+const DisplayTurn = styled.div`
+  display: flex;
+  padding: 8px 16px;
+`
+
+export default function RunGame(){
+  return(
+    <Container>
+      <Title>Tic Tac Toe</Title>
+      <DisplayTurn>
+
+      </DisplayTurn>
+    </Container>
+  )
+}
